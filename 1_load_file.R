a6afa18fbc0853b44a60551e156b18264027c065
qibaiqi <1178431277@qq.com>
2020-03-11 12:45:11 +0800

    利用parents的信息反向形成children.(parents : every term's parents list) (children : every term's children list) 形成children是为了下一步parents信息的合并

--- 1_load_file.R
+++ 1_load_file.R
@@ -45,6 +45,7 @@ get_parents <- function(start, end) {
 parents <- mapply(get_parents, loca_s, loca_d)
 names(parents) <- id
 
+
 #term与gene的关联信息文件
 ff <- readLines("gene_association.sgd")
 ff <- ff[-c(1:28)]## 去掉前几行介绍信息
@@ -53,61 +54,27 @@ ff <- t(as.data.frame(ff))## 变成了matrix
 gene_term <- data.frame(gene = ff[, 2], term = ff[, 5], stringsAsFactors = F)
 
 #提取term的注释信息：gene列表
-get_anno <- function(term) {
-  genes <- gene_term[gene_term$term == term, ]
+get_anno <- function(term, pool) {
+  genes <- pool[pool$term == term, ]
   genes <- unique(genes$gene)
   return(genes)
 }
 
-annotations <- lapply(node_bp, get_anno)
+annotations <- lapply(node_bp, get_anno, gene_term)
 names(annotations) <- node_bp
 
 
-save(list = ls(), file = "1_load_file.RData")
-
-#注释信息为空的行数
-term_remove_loca <- which(lapply(annotations, length) == 0)
-
-
-###########
-annotations <- annotations[-term_remove_loca]
-
-term_remove <- node_bp[term_remove_loca]
-
-node_bp <- node_bp[-term_remove_loca]
-
-parents <- lapply(term_remove, function(e) parents[[e]] <- NULL)
-
-#flip
+#根据变量parents反向形成children
 rep_times <- unname(unlist(lapply(parents, length)))
-rep_terms <- rep()
+rep_terms <- rep((as.character(id)), rep_times)
+unfold_par <- unlist(unname(parents))
+flip <- data.frame(parent = unfold_par, child = rep_terms, stringsAsFactors = F)
 
-
-###########
-rep_times <- unname(unlist(lapply(ff, length)))
-rep_terms <- rep(term_list, rep_times)
-flip_child <- data.frame(term = children, child = rep_terms)
-
-get_ont_children <- function(item) {
-  child <- subset(flip_child, flip_child$term == item, select = c("child"))
-  child <- as.character(unique(child)$child)
+get_children <- function(term, pool) {
+  children <- pool[pool$parent == term, ]
+  children <- unique(children$child)
+  return(children)
 }
-ont_children <- lapply(term_list, get_ont_children)
-names(ont_children) <- term_list
-ont_children <- ont_children[lapply(ont_children, length) > 0]
 
-
-
-
-
-
-
-
-#[Term]
-#id: GO:0000018
-#name: regulation of DNA recombination
-#namespace: biological_process
-#def: "Any process that modulates ...
-#subset: gosubset_prok
-#is_a: GO:0051052 ! regulation of DNA metabolic process
-#relationship: regulates GO:0006310 ! DNA recombination
+children <- lapply(id, get_children, flip)
+names(children) <- id
